6781780d7f0bc052337e2cd4ee12dd09e47c208a
Stefano Verna <s.verna@cantierecreativo.net>
2018-10-31 14:11:24 +0100

    Fix

--- conditional-fields/src/index.js
+++ conditional-fields/src/index.js
@@ -1,9 +1,29 @@
+import values from 'object.values';
+
 window.DatoCmsPlugin.init((plugin) => {
   const slaveFields = plugin.parameters.instance.slaveFields.split(/\s*,\s*/);
+  const masterField = plugin.field;
 
   function toggleFields(value) {
-    slaveFields.forEach((slaveField) => {
-      plugin.toggleField(slaveField, value);
+    slaveFields.forEach((slaveFieldApiKey) => {
+      const slaveField = values(plugin.fields)
+        .find(field => field.attributes.api_key === slaveFieldApiKey);
+
+      const slavePath = plugin.parentFieldId
+        ? `${plugin.fieldPath.replace(/.[^.]*$/, '')}.${slaveFieldApiKey}`
+        : slaveFieldApiKey;
+
+      if (masterField.attributes.localized) {
+        if (slaveField.attributes.localized) {
+          plugin.toggleField(`${slavePath}.${plugin.locale}`, value);
+        }
+      } else if (slaveField.attributes.localized) {
+        plugin.site.attributes.locales.forEach((locale) => {
+          plugin.toggleField(`${slavePath}.${locale}`, value);
+        });
+      } else {
+        plugin.toggleField(slavePath, value);
+      }
     });
   }
 
