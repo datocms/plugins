442860d01217a960248a864cbbd7e168f50f8e3e
Irene Oppo <i.oppo@datocms.com>
2021-10-08 11:26:13 +0200

    Created Tiny MCE plugin

---
+++ tiny-mce/webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackTagsPlugin = require("html-webpack-tags-plugin");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+module.exports = {
+  entry: __dirname + "/src/index.js",
+  mode: process.env.NODE_ENV,
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+  devtool: "source-map",
+  devServer: {
+    contentBase: "./",
+    disableHostCheck: true,
+    public: "http://localhost:5000",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: __dirname + "/src",
+        loader: "eslint-loader",
+        enforce: "pre",
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: { loader: "babel-loader" },
+      },
+      {
+        test: /\.sass$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "DatoCMS Plugin",
+      minify: isProduction,
+    }),
+    new HtmlWebpackTagsPlugin({
+      append: false,
+      publicPath: "",
+      tags: [
+        "https://unpkg.com/datocms-plugins-sdk@0.1.1/dist/sdk.js",
+        "https://unpkg.com/datocms-plugins-sdk@0.1.1/dist/sdk.css",
+      ],
+    }),
+  ].filter(Boolean),
+};
