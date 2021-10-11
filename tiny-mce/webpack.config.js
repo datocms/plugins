ffedc9210306e7229131416f72fc8fbabba7772d
Irene Oppo <i.oppo@datocms.com>
2021-10-11 10:44:06 +0200

    Configured webpack

--- tiny-mce/webpack.config.js
+++ tiny-mce/webpack.config.js
@@ -1,20 +1,24 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const HtmlWebpackTagsPlugin = require("html-webpack-tags-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const isProduction = process.env.NODE_ENV === "production";
-const path = __dirname + "/dist";
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  entry: __dirname + "/src/index.js",
+  entry: `${__dirname}/src/index.js`,
   mode: process.env.NODE_ENV,
-  output: {
-    path,
-    filename: "bundle.js",
+  resolve: {
+    extensions: ['.js'],
   },
-  devtool: "source-map",
+  output: {
+    path: `${__dirname}/dist`,
+    filename: '[name].js',
+    clean: true,
+  },
+  devtool: 'source-map',
   devServer: {
     static: {
-      directory: path,
+      directory: `${__dirname}/dist`,
     },
     compress: true,
     port: 5000,
@@ -23,33 +27,45 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        include: __dirname + "/src",
-        loader: "eslint-loader",
-        enforce: "pre",
+        include: `${__dirname}/src`,
+        loader: 'eslint-loader',
+        enforce: 'pre',
       },
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        use: { loader: "babel-loader" },
+        use: { loader: 'babel-loader' },
       },
       {
-        test: /\.sass$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "DatoCMS Plugin",
+      title: 'DatoCMS Plugin',
       minify: isProduction,
     }),
     new HtmlWebpackTagsPlugin({
       append: false,
-      publicPath: "",
+      publicPath: '',
       tags: [
-        "https://unpkg.com/datocms-plugins-sdk@0.1.1/dist/sdk.js",
-        "https://unpkg.com/datocms-plugins-sdk@0.1.1/dist/sdk.css",
+        'https://unpkg.com/datocms-plugins-sdk@0.1.2/dist/sdk.js',
+        'https://unpkg.com/datocms-plugins-sdk@0.1.2/dist/sdk.css',
       ],
     }),
+    new MiniCssExtractPlugin(),
   ].filter(Boolean),
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        tinymceVendor: {
+          test: /[\\/]node_modules[\\/](tinymce)[\\/](.*js|.*skin.css)|[\\/]plugins[\\/]/,
+          name: 'tinymce',
+        },
+      },
+    },
+  },
 };
