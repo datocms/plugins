1816f6c79664dff313666040c278bbe28abe6bf3
Stefano Verna <s.verna@datocms.com>
2021-11-30 15:54:22 +0100

    Upgrade associations

--- seo-readability-analysis/src/index.tsx
+++ seo-readability-analysis/src/index.tsx
@@ -1,4 +1,4 @@
-import { connect } from 'datocms-plugin-sdk';
+import { connect, FieldAppearanceChange } from 'datocms-plugin-sdk';
 import { render } from './utils/render';
 import { Parameters } from './types';
 import ConfigScreen from './entrypoints/ConfigScreen';
@@ -9,6 +9,40 @@ import './style.sass';
 const initialHeight = 200;
 
 connect({
+  async onBoot(ctx) {
+    if (
+      ctx.plugin.attributes.parameters
+        .setSeoReadabilityAnalysisFieldExtensionId ||
+      !ctx.currentRole.meta.final_permissions.can_edit_schema
+    ) {
+      return;
+    }
+
+    const fields = await ctx.loadFieldsUsingPlugin();
+
+    await Promise.all(
+      fields.map(async (field) => {
+        const { appearance } = field.attributes;
+        const changes: FieldAppearanceChange[] = [];
+
+        if (appearance.editor === ctx.plugin.id) {
+          changes.push({
+            operation: 'updateEditor',
+            newFieldExtensionId: 'seoReadabilityAnalysis',
+          });
+        }
+
+        if (changes.length > 0) {
+          await ctx.updateFieldAppearance(field.id, changes);
+        }
+      }),
+    );
+
+    await ctx.updatePluginParameters({
+      ...ctx.plugin.attributes.parameters,
+      setSeoReadabilityAnalysisFieldExtensionId: true,
+    });
+  },
   renderConfigScreen(ctx) {
     return render(<ConfigScreen ctx={ctx} />);
   },
