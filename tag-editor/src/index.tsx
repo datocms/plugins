8186186b7f54462ff8fd912464784bda6fac307a
Irene Oppo <i.oppo@datocms.com>
2021-12-07 10:45:20 +0100

    onBoot

--- tag-editor/src/index.tsx
+++ tag-editor/src/index.tsx
@@ -5,6 +5,40 @@ import { PluginAttributes } from "datocms-plugin-sdk/dist/types/SiteApiSchema";
 import FieldExtension from "./entrypoints/FieldExtension";
 
 connect({
+  async onBoot(ctx) {
+    if (ctx.plugin.attributes.parameters.upgradedFromLegacyPlugin) {
+      return;
+    }
+
+    if (!ctx.currentRole.meta.final_permissions.can_edit_schema) {
+      return;
+    }
+
+    const fields = await ctx.loadFieldsUsingPlugin();
+
+    await Promise.all(
+      fields.map(async (field) => {
+        if (
+          field.attributes.appearance.editor === ctx.plugin.id &&
+          field.attributes.appearance.field_extension === "tagEditor"
+        ) {
+          await ctx.updateFieldAppearance(field.id, [
+            {
+              operation: "updateEditor",
+              newFieldExtensionId: "tagEditor",
+            },
+          ]);
+        }
+      })
+    );
+
+    ctx.notice("Plugin upgraded successfully!");
+
+    ctx.updatePluginParameters({
+      ...ctx.plugin.attributes.parameters,
+      upgradedFromLegacyPlugin: true,
+    });
+  },
   manualFieldExtensions() {
     return [
       {
