72408a3576ca63d4ee87ee1ebf11a96834e4acfb
Irene Oppo <i.oppo@datocms.com>
2021-12-17 12:14:31 +0100

    Removed legacy

--- rich-text-tinymce/src/index.tsx
+++ rich-text-tinymce/src/index.tsx
@@ -1,4 +1,4 @@
-import { connect } from "datocms-plugin-sdk";
+import { connect, OnBootCtx } from "datocms-plugin-sdk";
 import { render } from "./utils/render";
 import { PluginAttributes } from "datocms-plugin-sdk/dist/types/SiteApiSchema";
 import FieldExtension from "./entrypoints/FieldExtension";
@@ -6,6 +6,36 @@ import ConfigScreen from "./entrypoints/ConfigScreen";
 import "datocms-react-ui/styles.css";
 
 connect({
+  async onBoot(ctx: OnBootCtx) {
+    if (
+      !ctx.currentRole.meta.final_permissions.can_edit_schema ||
+      ctx.plugin.attributes.parameters.migratedFromLegacyPlugin
+    ) {
+      return;
+    }
+
+    const fields = await ctx.loadFieldsUsingPlugin();
+
+    await Promise.all(
+      fields.map(async (field) => {
+        if (field.attributes.appearance.editor === ctx.plugin.id) {
+          await ctx.updateFieldAppearance(field.id, [
+            {
+              operation: "updateEditor",
+              newFieldExtensionId: "tinyMce",
+            },
+          ]);
+        }
+      })
+    );
+
+    await ctx.updatePluginParameters({
+      ...ctx.plugin.attributes.parameters,
+      migratedFromLegacyPlugin: true,
+    });
+
+    ctx.notice("Plugin upgraded successfully!");
+  },
   renderConfigScreen(ctx) {
     return render(<ConfigScreen ctx={ctx} />);
   },
