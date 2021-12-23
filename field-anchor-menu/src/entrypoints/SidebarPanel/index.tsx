2848185e91340c296692a5a986c6f7292c7909f4
Stefano Verna <s.verna@datocms.com>
2021-12-23 16:53:08 +0100

    Cover and video

--- field-anchor-menu/src/entrypoints/SidebarPanel/index.tsx
+++ field-anchor-menu/src/entrypoints/SidebarPanel/index.tsx
@@ -1,4 +1,5 @@
 import { RenderItemFormSidebarPanelCtx } from 'datocms-plugin-sdk';
+import { Fieldset } from 'datocms-plugin-sdk/dist/types/SiteApiSchema';
 import { Canvas } from 'datocms-react-ui';
 import styles from './styles.module.css';
 
@@ -11,23 +12,61 @@ const FieldAnchorMenu = ({ ctx }: PropTypes) => {
     ({ id }) => ctx.fields[id]!,
   );
 
+  const topLevelStuff = [
+    ...(Object.values(ctx.fieldsets) as Fieldset[]).filter(
+      (fieldset) =>
+        fieldset.relationships.item_type.data.id === ctx.itemType.id,
+    ),
+    ...allFieldsToShow.filter((field) => !field.relationships.fieldset.data),
+  ].sort((a, b) => a.attributes.position - b.attributes.position);
+
   return (
     <Canvas ctx={ctx}>
-      {allFieldsToShow.map((field) => (
-        <div
-          className={styles.link}
-          onClick={() => {
-            ctx.scrollToField(
-              `${field.attributes.api_key}${
-                field.attributes.localized ? ctx.locale : ''
-              }`,
-            );
-          }}
-          key={field.id}
-        >
-          {field.attributes.label}
-        </div>
-      ))}
+      {topLevelStuff.map((fieldOrFieldset) =>
+        fieldOrFieldset.type === 'field' ? (
+          <div
+            className={styles.link}
+            onClick={() => {
+              ctx.scrollToField(
+                `${fieldOrFieldset.attributes.api_key}${
+                  fieldOrFieldset.attributes.localized ? ctx.locale : ''
+                }`,
+              );
+            }}
+            key={fieldOrFieldset.id}
+          >
+            {fieldOrFieldset.attributes.label}
+          </div>
+        ) : (
+          <div className={styles.group} key={fieldOrFieldset.id}>
+            <div className={styles.groupName}>
+              {fieldOrFieldset.attributes.title}
+            </div>
+            <div className={styles.groupChildren}>
+              {allFieldsToShow
+                .filter(
+                  (f) =>
+                    f.relationships.fieldset.data?.id === fieldOrFieldset.id,
+                )
+                .map((fieldOrFieldset) => (
+                  <div
+                    className={styles.link}
+                    onClick={() => {
+                      ctx.scrollToField(
+                        `${fieldOrFieldset.attributes.api_key}${
+                          fieldOrFieldset.attributes.localized ? ctx.locale : ''
+                        }`,
+                      );
+                    }}
+                    key={fieldOrFieldset.id}
+                  >
+                    {fieldOrFieldset.attributes.label}
+                  </div>
+                ))}
+            </div>
+          </div>
+        ),
+      )}
     </Canvas>
   );
 };
