82b819fef2af71bc1bf8379cf141f1b505f8a481
Stefano Verna <s.verna@datocms.com>
2022-02-18 12:29:09 +0100

    Working

--- rich-text-tinymce/src/entrypoints/FieldExtension/index.tsx
+++ rich-text-tinymce/src/entrypoints/FieldExtension/index.tsx
@@ -1,41 +1,38 @@
-import { RenderFieldExtensionCtx } from "datocms-plugin-sdk";
-import { Canvas } from "datocms-react-ui";
-import get from "lodash/get";
-/* Import TinyMCE */
-// To know all the available TinyMCE options and plugins go to https://www.tiny.cloud/docs/
+import { RenderFieldExtensionCtx } from 'datocms-plugin-sdk';
+import { Canvas } from 'datocms-react-ui';
+import get from 'lodash/get';
+import tinymce from 'tinymce/tinymce';
+import { Editor as ReactEditor } from '@tinymce/tinymce-react';
+import imgixThumbUrl from '../../utils/imgixThumbUrl';
+import { Editor } from 'tinymce';
 
-/* Import TinyMCE */
-import tinymce, { Editor } from "tinymce";
+(global as any).tinymce = tinymce;
 
-/* Default icons are required for TinyMCE 5.3 or above */
-import "tinymce/icons/default";
-
-/* A theme is also required */
-import "tinymce/themes/silver";
-
-/* Import the skin */
-import "tinymce/skins/ui/oxide/skin.css";
-
-/* Import plugins */
-import "tinymce/plugins/image";
-import "tinymce/plugins/advlist";
-import "tinymce/plugins/code";
-import "tinymce/plugins/emoticons";
-import "tinymce/plugins/emoticons/js/emojis";
-import "tinymce/plugins/link";
-import "tinymce/plugins/lists";
-import "tinymce/plugins/paste";
-import "tinymce/plugins/table";
-import "tinymce/plugins/autoresize";
-import imgixThumbUrl from "../../utils/imgixThumbUrl";
-
-import s from "./styles.module.css";
+require('tinymce/icons/default');
+require('tinymce/themes/silver');
+require('tinymce/skins/ui/oxide/skin.css');
+require('tinymce/plugins/image');
+require('tinymce/plugins/advlist');
+require('tinymce/plugins/code');
+require('tinymce/plugins/emoticons');
+require('tinymce/plugins/emoticons/js/emojis');
+require('tinymce/plugins/link');
+require('tinymce/plugins/lists');
+require('tinymce/plugins/paste');
+require('tinymce/plugins/table');
+require('tinymce/plugins/autoresize');
 
 type Props = {
   ctx: RenderFieldExtensionCtx;
 };
 
 export default function FieldExtension({ ctx }: Props) {
+  const value = (get(ctx.formValues, ctx.fieldPath) as string | null) || '';
+
+  const handleChange = (newValue: string) => {
+    ctx.setFieldValue(ctx.fieldPath, newValue);
+  };
+
   const initialize = (editor: Editor) => {
     const handleDatoImages = () => {
       // Handles inserting Dato image in the HTML editor
@@ -44,14 +41,14 @@ export default function FieldExtension({ ctx }: Props) {
           files.forEach((file) => {
             const metadata = file.attributes.default_field_metadata[ctx.locale];
 
-            let text = "<img ";
+            let text = '<img ';
 
             if (metadata.alt) {
-              text += `alt="${metadata.alt || ""}" `;
+              text += `alt="${metadata.alt || ''}" `;
             }
 
             if (metadata.title) {
-              text += `title="${metadata.title || ""}" `;
+              text += `title="${metadata.title || ''}" `;
             }
 
             text += `src="${imgixThumbUrl({
@@ -64,61 +61,58 @@ export default function FieldExtension({ ctx }: Props) {
       });
     };
 
-    editor.on("init", () => {
-      const initialValue = get(ctx.formValues, ctx.fieldPath) as string;
-      editor.setContent(initialValue);
-    });
-
-    editor.on("change", () => {
-      // Sets the plugin value on blur
+    editor.on('change', () => {
+      console.log(editor.getContent());
       ctx.setFieldValue(ctx.fieldPath, editor.getContent());
     });
 
-    editor.ui.registry.addButton("customimage", {
-      icon: "image",
-      tooltip: "Insert image...",
+    editor.ui.registry.addButton('customimage', {
+      icon: 'image',
+      tooltip: 'Insert image...',
       onAction: handleDatoImages,
     });
 
-    editor.ui.registry.addButton("replaceimage", {
-      icon: "browse",
-      tooltip: "Replace image",
+    editor.ui.registry.addButton('replaceimage', {
+      icon: 'browse',
+      tooltip: 'Replace image',
       onAction: handleDatoImages,
     });
 
-    editor.ui.registry.addContextToolbar("imagealignment", {
+    editor.ui.registry.addContextToolbar('imagealignment', {
       // Shows image replacement options when selecting image
       predicate: (node) => {
-        if (node.nodeName.toLowerCase() === "img") {
+        if (node.nodeName.toLowerCase() === 'img') {
           return true;
         }
         return false;
       },
 
-      items: "replaceimage image",
-      position: "node",
-      scope: "node",
+      items: 'replaceimage image',
+      position: 'node',
+      scope: 'node',
     });
   };
 
-  tinymce.init({
-    selector: "#tinymce__container",
-    plugins: "image advlist code emoticons link lists table autoresize",
-    toolbar:
-      "undo redo | formatselect | " +
-      "bold italic backcolor | link customimage |" +
-      "alignleft aligncenter " +
-      "alignright alignjustify | bullist numlist outdent indent | " +
-      "removeformat | emoticons",
-    content_style:
-      "body { font-family:Helvetica,Arial,sans-serif; font-size:16px }",
-    setup: initialize,
-    autoresize_bottom_margin: 10,
-  });
-
   return (
     <Canvas ctx={ctx}>
-      <div id="tinymce__container" className={s.tinymce__container} />
+      <ReactEditor
+        disabled={ctx.disabled}
+        init={{
+          plugins: 'image advlist code emoticons link lists table autoresize',
+          toolbar:
+            'undo redo | formatselect | ' +
+            'bold italic backcolor | link customimage |' +
+            'alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | emoticons',
+          content_style:
+            'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }',
+          setup: initialize,
+          autoresize_bottom_margin: 10,
+        }}
+        value={value}
+        onEditorChange={handleChange}
+      />
     </Canvas>
   );
 }
--- rich-text-tinymce/src/index.tsx
+++ rich-text-tinymce/src/index.tsx
@@ -1,9 +1,8 @@
-import { connect, OnBootCtx } from "datocms-plugin-sdk";
-import { render } from "./utils/render";
-import { PluginAttributes } from "datocms-plugin-sdk/dist/types/SiteApiSchema";
-import FieldExtension from "./entrypoints/FieldExtension";
-import ConfigScreen from "./entrypoints/ConfigScreen";
-import "datocms-react-ui/styles.css";
+import { connect, OnBootCtx } from 'datocms-plugin-sdk';
+import { render } from './utils/render';
+import { PluginAttributes } from 'datocms-plugin-sdk/dist/types/SiteApiSchema';
+import FieldExtension from './entrypoints/FieldExtension';
+import 'datocms-react-ui/styles.css';
 
 connect({
   async onBoot(ctx: OnBootCtx) {
@@ -21,12 +20,12 @@ connect({
         if (field.attributes.appearance.editor === ctx.plugin.id) {
           await ctx.updateFieldAppearance(field.id, [
             {
-              operation: "updateEditor",
-              newFieldExtensionId: "tinyMce",
+              operation: 'updateEditor',
+              newFieldExtensionId: 'tinymce',
             },
           ]);
         }
-      })
+      }),
     );
 
     await ctx.updatePluginParameters({
@@ -34,18 +33,15 @@ connect({
       migratedFromLegacyPlugin: true,
     });
 
-    ctx.notice("Plugin upgraded successfully!");
-  },
-  renderConfigScreen(ctx) {
-    return render(<ConfigScreen ctx={ctx} />);
+    ctx.notice('Plugin upgraded successfully!');
   },
   manualFieldExtensions() {
     return [
       {
-        id: "tinyMce",
-        name: "Tiny MCE",
-        type: "editor",
-        fieldTypes: ["text"] as NonNullable<PluginAttributes["field_types"]>,
+        id: 'tinymce',
+        name: 'TinyMCE',
+        type: 'editor',
+        fieldTypes: ['text'] as NonNullable<PluginAttributes['field_types']>,
       },
     ];
   },
