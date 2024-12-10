0aa8e9ef9faf6672a6a4a624e4fede50fcd503d8
Stefano Verna <s.verna@datocms.com>
2024-12-10 16:27:45 +0100

    wip

--- upgrade.sh
+++ upgrade.sh
@@ -14,7 +14,7 @@ cp /Users/stefanoverna/trash/my-first-plugin-xxx/tsconfig.node.json .
 cp /Users/stefanoverna/trash/my-first-plugin-xxx/vite.config.ts .
 
 npx json -I -f package.json -e "delete this.scripts.test; delete this.scripts.eject; delete this.scripts.start; this.scripts.dev='vite'; this.scripts.build='tsc -b && vite build'; this.scripts.preview='vite preview';"
-npx json -I -f package.json -e "this.files = this.files.map(x => x === 'build' ? 'dist' : x)"
+npx json -I -f package.json -e "if (this.files) { this.files = this.files.map(x => x === 'build' ? 'dist' : x); } else { this.files = ['dist', 'docs'] }"
 npx json -I -f package.json -e "for (const pkg of ['@types/node', 'react-scripts', 'typescript']) { delete this.dependencies[pkg]; delete this.devDependencies[pkg]; }"
 npx json -I -f package.json -e "this.devDependencies['@vitejs/plugin-react'] = '^4.3.1'; this.devDependencies['globals'] = '^15.9.0'; this.devDependencies['typescript'] = '^5.5.3'; this.devDependencies['vite'] = '^5.4.1';"
 npx json -I -f package.json -e "this.datoCmsPlugin.entryPoint = 'dist/index.html';"
