1747a01d9ffb49b28f3a6993e0a9c8ec24105668
Stefano Verna <s.verna@datocms.com>
2024-12-10 16:33:28 +0100

    wip

--- upgrade.sh
+++ upgrade.sh
@@ -3,15 +3,15 @@ rm public/index.html
 
 mv src/index.tsx src/main.tsx
 
-cp /Users/stefanoverna/trash/my-first-plugin-xxx/src/vite-env.d.ts src
+wget https://raw.githubusercontent.com/datocms/datocms-plugin-template/refs/heads/main/src/vite-env.d.ts -P src
 
-cp /Users/stefanoverna/trash/my-first-plugin-xxx/index.html .
-cp /Users/stefanoverna/trash/my-first-plugin-xxx/.gitignore .
+wget https://raw.githubusercontent.com/datocms/datocms-plugin-template/refs/heads/main/index.html
+wget https://raw.githubusercontent.com/datocms/datocms-plugin-template/refs/heads/main/.gitignore
 
-cp /Users/stefanoverna/trash/my-first-plugin-xxx/tsconfig.app.json .
-cp /Users/stefanoverna/trash/my-first-plugin-xxx/tsconfig.json .
-cp /Users/stefanoverna/trash/my-first-plugin-xxx/tsconfig.node.json .
-cp /Users/stefanoverna/trash/my-first-plugin-xxx/vite.config.ts .
+wget https://raw.githubusercontent.com/datocms/datocms-plugin-template/refs/heads/main/tsconfig.app.json
+wget https://raw.githubusercontent.com/datocms/datocms-plugin-template/refs/heads/main/tsconfig.json
+wget https://raw.githubusercontent.com/datocms/datocms-plugin-template/refs/heads/main/tsconfig.node.json
+wget https://raw.githubusercontent.com/datocms/datocms-plugin-template/refs/heads/main/vite.config.ts
 
 npx json -I -f package.json -e "delete this.scripts.test; delete this.scripts.eject; delete this.scripts.start; this.scripts.dev='vite'; this.scripts.build='tsc -b && vite build'; this.scripts.preview='vite preview';"
 npx json -I -f package.json -e "if (this.files) { this.files = this.files.map(x => x === 'build' ? 'dist' : x); } else { this.files = ['dist', 'docs'] }"
