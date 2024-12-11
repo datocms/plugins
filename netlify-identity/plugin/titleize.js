04297254291bc3aea111c1e5dcb4e4c71374445f
Stefano Verna <s.verna@datocms.com>
2024-12-11 15:03:52 +0100

    Remove netlify identity plugin

--- netlify-identity/plugin/titleize.js
+++
@@ -1,7 +0,0 @@
-const titleizeWord = string => (
-  string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
-);
-
-export default function titleize(sentence) {
-  return sentence.split(/[ _-]+/).map(titleizeWord).join(' ');
-}
