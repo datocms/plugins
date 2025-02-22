74a4f8a378cc16a677e8f4f73fa40da8f7fdc19e
Marcelo Finamor Vieira <marcelofinamorvieira@gmail.com>
2025-02-22 17:05:39 +0100

    Add manual field extension for Lorem Ipsum generation

--- lorem-ipsum/src/main.tsx
+++ lorem-ipsum/src/main.tsx
@@ -1,4 +1,8 @@
-import { connect, type FieldDropdownActionsCtx, type ExecuteFieldDropdownActionCtx } from 'datocms-plugin-sdk';
+import {
+  connect,
+  type FieldDropdownActionsCtx,
+  type ExecuteFieldDropdownActionCtx,
+} from 'datocms-plugin-sdk';
 import { render } from './utils/render';
 import ConfigScreen from './entrypoints/ConfigScreen';
 import 'datocms-react-ui/styles.css';
@@ -12,15 +16,20 @@ connect({
     const pluginParams = ctx.plugin.attributes.parameters;
     if (pluginParams && Array.isArray(pluginParams.autoApplyRules)) {
       for (const rule of pluginParams.autoApplyRules) {
-        if (rule.fieldTypes && rule.fieldTypes.includes(field.attributes.field_type)) {
+        if (
+          rule.fieldTypes &&
+          rule.fieldTypes.includes(field.attributes.field_type)
+        ) {
           try {
             const regex = new RegExp(rule.apiKeyRegexp);
             if (regex.test(field.attributes.api_key)) {
-              return [{
-                id: "loremIpsum",
-                label: "Generate Lorem Ipsum",
-                icon: "font"
-              }];
+              return [
+                {
+                  id: 'loremIpsum',
+                  label: 'Generate Lorem Ipsum',
+                  icon: 'font',
+                },
+              ];
             }
           } catch (e) {
             // Skip rule if regex is invalid
@@ -28,13 +37,41 @@ connect({
         }
       }
     }
+    // Also add dropdown action if the manual field extension is applied
+    if (
+      field.attributes.appearance?.addons?.some(
+        (addon) => addon.field_extension === 'loremIpsum'
+      )
+    ) {
+      return [
+        {
+          id: 'loremIpsum',
+          label: 'Generate Lorem Ipsum',
+          icon: 'font',
+        },
+      ];
+    }
     return [];
   },
-  async executeFieldDropdownAction(actionId, ctx: ExecuteFieldDropdownActionCtx) {
-    if (actionId === "loremIpsum") {
+  async executeFieldDropdownAction(
+    actionId,
+    ctx: ExecuteFieldDropdownActionCtx
+  ) {
+    if (actionId === 'loremIpsum') {
       const generated = generateDummyText(ctx.field);
       await ctx.setFieldValue(ctx.fieldPath, generated);
-      ctx.notice("Lorem Ipsum generated!");
+      ctx.notice('Lorem Ipsum generated!');
     }
+  },
+  manualFieldExtensions() {
+    return [
+      {
+        id: 'loremIpsum',
+        name: 'Lorem Ipsum',
+        type: 'addon',
+        fieldTypes: ['text', 'string', 'structured_text'],
+        configurable: true,
+      },
+    ];
   },
 });
