2ea5e40667d04151d126ce9d3638a9ba422fb01a
Marcelo Finamor Vieira <marcelofinamorvieira@gmail.com>
2025-09-23 16:17:45 +0200

    types

--- import-export-schema/src/types/lodash-es.d.ts
+++ import-export-schema/src/types/lodash-es.d.ts
@@ -1,18 +1,5 @@
 declare module 'lodash-es' {
-  // Minimal ambient module to satisfy TS in this project. For richer types, install @types/lodash-es.
-  export const get: any;
-  export const set: any;
-  export const pick: any;
-  export const omit: any;
-  export const intersection: any;
-  export const keyBy: any;
-  export const sortBy: any;
-  export const find: any;
-  export const without: any;
-  export const map: any;
-  export const defaults: any;
-  export const mapValues: any;
-  export const groupBy: any;
-  export const isEqual: any;
-  export const cloneDeep: any;
+  export * from 'lodash';
+  import _ from 'lodash';
+  export default _;
 }
